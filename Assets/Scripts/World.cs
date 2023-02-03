a63efd96c0556daac14508cae5af8c880e1f5d92
Ermith <f.s.ermith@gmail.com>
2023-02-03 19:50:21 +0100

    The tile renders

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -2,44 +2,113 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile
+public abstract class Tile
 {
     public int X;
     public int Y;
-    List<Sprite> Sprites;
+    protected Dictionary<string, Sprite> _sprites;
 
-    GameObject Create()
+    protected Tile(Dictionary<string, Sprite> sprites)
     {
-        if (Sprites.Count == 1)
-        {
-            GameObject tile = new GameObject();
-            var spriteRenderer = tile.AddComponent<SpriteRenderer>();
-            spriteRenderer.sprite = Sprites[0];
-            return GameObject.Instantiate(tile);
-        }
+        _sprites = sprites;
+    }
 
-        // just for now
-        return null;
+    private GameObject CreateCorner(string type, string corner, string cornerType)
+    {
+        if (cornerType.Length == 0) cornerType += "-";
+
+        GameObject spriteObject = new GameObject();
+
+        string spriteName = $"{type}{corner}{cornerType}";
+        spriteObject.AddComponent<SpriteRenderer>().sprite = _sprites[spriteName];
+        spriteObject.name = spriteName;
+
+        return spriteObject;
+    }
+
+    protected GameObject CreateSpriteObject(
+        string type,
+        string _00 = "",
+        string _01 = "",
+        string _10 = "",
+        string _11 = "")
+    {
+        GameObject sprite00 = CreateCorner(type, "00", _00);
+        GameObject sprite01 = CreateCorner(type, "01", _01);
+        GameObject sprite10 = CreateCorner(type, "10", _10);
+        GameObject sprite11 = CreateCorner(type, "11", _11);
+
+        sprite01.transform.position += Vector3.up * 0.5f;
+        sprite10.transform.position += Vector3.right * 0.5f;
+        sprite11.transform.position += Vector3.right * 0.5f + Vector3.up * 0.5f;
+
+        GameObject fatherObject = new GameObject();
+
+        sprite00.transform.parent = fatherObject.transform;
+        sprite01.transform.parent = fatherObject.transform;
+        sprite10.transform.parent = fatherObject.transform;
+        sprite11.transform.parent = fatherObject.transform;
+
+        return fatherObject;
+    }
+
+    public abstract GameObject UpdateSprite(List<Tile> tiles);
+}
+
+public class GroundTile : Tile
+{
+    public GroundTile(Dictionary<string, Sprite> sprites) : base(sprites)
+    {
+    }
+
+    public override GameObject UpdateSprite(List<Tile> tiles)
+    {
+        int left = X - 1;
+        int top = Y - 1;
+        int right = X + 1;
+        int bottom = Y + 1;
+
+        // TODO: CHECK NEIGHBORS
+
+        return CreateSpriteObject("ground", _00: "-corner");
     }
 }
 
 public class World : MonoBehaviour
 {
-    private Camera _camera;
+    public Camera _camera;
     private List<Tile> _tiles;
-    private float _offsetX;
-    private float _offsetY;
+    private float _offsetX; // based on camera witdth
+    private float _offsetY; // camera position
 
     // Start is called before the first frame update
     void Start()
     {
         _offsetX = _camera.orthographicSize - Mathf.Floor(_camera.orthographicSize);
         _offsetY = 0;
+
+        var sprites = LoadSprites();
+
+        var groundTile = new GroundTile(sprites);
+        var groundObject = groundTile.UpdateSprite(_tiles);
+        groundObject.transform.parent = this.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private Dictionary<string, Sprite> LoadSprites()
+    {
+        var sprites = new Dictionary<string, Sprite>();
+        var groundSprites = Resources.LoadAll<Sprite>("Sprites/Ground");
+        foreach (var sprite in groundSprites)
+        {
+            sprites.Add(sprite.name, sprite);
+        }
+
+        return sprites;
     }
 }
